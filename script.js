e1d5bee5b60d8a166bcf5cd02927555746326b50
BCarrico <BCarrico89@gmail.com>
2022-06-16 17:11:39 -0700

    added 3 sizes, no slider, buttons only, clear canvas per size

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gridContainer = document.querySelector('.gridContainer');
-
+let currentGridNum = 16;
 
 makeGrid(16);
 colorCells();
+
 function makeGrid (gridNum){
     for (r = 0; r < gridNum; r++){
         let div1 = document.createElement('div');
@@ -24,10 +25,8 @@ function makeGrid (gridNum){
 
 let newGrid = document.querySelector(".newGrid");
 newGrid.addEventListener('click', () => {
-    let gridNum = window.prompt("How big would you like your grid? (16 - 100)")
     eraseGrid();
-    makeGrid(gridNum);
-    colorCells();
+    createGrid(currentGridNum); /*need to fix*/
 })
 
 function eraseGrid(){
@@ -42,4 +41,28 @@ hoverEffect.forEach(cell => {
         console.log('cell entered');
     });
 });
+}
+
+let grid16button = document.querySelector(".grid16");
+grid16button.addEventListener('click', () => {
+    createGrid(16);
+    currentGridNum = 16;
+})
+
+let grid32button = document.querySelector(".grid32");
+grid32button.addEventListener('click', () => {
+    createGrid(32);
+    currentGridNum = 32;
+})
+
+let grid64button = document.querySelector(".grid64");
+grid64button.addEventListener('click', () => {
+    createGrid(64);
+    currentGridNum = 64;
+})
+
+function createGrid (int){
+    eraseGrid();
+    makeGrid(int);
+    colorCells();
 }
